cebbef916ebfff30e2644e1f577ec12dd3ca46d2
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-19 22:10:23 +0530

    added @next/fonts more ui elements

--- src/app/Contacts.tsx
+++ src/app/Contacts.tsx
@@ -1,14 +1,19 @@
 "use client";
+
+import AddFrnd from "./AddFrnd";
+import ContactHeader from "./ContactHeader";
+import Status from "./Status";
+
 export default function Contacts() {
   return (
-    <div
-      className={` w-[35%]    border-4 border-[#1E1E1E] border-r-2  flex flex-col`}
-    >
+    <>
       <div
-        className={`flex items-center justify-start  font-semibold text-xl  bg-[#8F8FCA]  px-4 pl-6 pb-10 py=4 `}
+        className={` w-[35%]    border-4 border-[#1E1E1E] border-r-2  flex flex-col gap-2`}
       >
-        PingMe
+        <ContactHeader />
+        <AddFrnd />
+        <Status />
       </div>
-    </div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        kanit: ["Kanit", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
