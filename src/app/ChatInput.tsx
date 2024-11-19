ed10185300aebeebc2636c18cc12492a70e6d9d1
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-19 22:09:34 +0530

    new color added for send

--- src/app/ChatInput.tsx
+++ src/app/ChatInput.tsx
@@ -30,10 +30,10 @@ function ChatInput() {
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          height="24px"
+          height="35px"
           viewBox="0 -960 960 960"
-          width="24px"
-          fill="#8F8FCA"
+          width="35px"
+          fill="#CACA8F"
         >
           <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
         </svg>
