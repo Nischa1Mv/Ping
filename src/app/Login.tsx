24878ca17fd4ec676ca76363d303dd0d11d82a50
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-20 19:25:20 +0530

    added login page , click login and done

---
+++ src/app/Login.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Kanit } from "next/font/google";
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["600"],
+  style: ["italic"],
+});
+interface LoginProps {
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+}
+function Login({ setIsLogin }: LoginProps) {
+  return (
+    <div className="h-screen bg-[#191a22] flex justify-center items-center">
+      <form className="flex flex-col justify-center items-center gap-7 w-[35%] py-10 rounded-lg bg-[#21222e] border-[#1E1E1E]">
+        <span
+          className={`${kanit.className} text-6xl gap-1 flex text-[#CACA8F]`}
+        >
+          Ping
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="currentColor"
+          >
+            <path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
+          </svg>
+        </span>
+
+        <div className="text-xl font-bold text-[#8F8FCA] italic px-4 gap-2 items-center flex">
+          Login
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#8F8FCA"
+          >
+            <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
+          </svg>
+        </div>
+
+        <div>
+          <input
+            className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 border-[#dedfeb] focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <input
+            className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <input
+            className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
+            type="password"
+            placeholder="Confirm Password"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input type="checkbox" />
+          <label htmlFor="checkbox" className="text-sm">
+            I accept the Terms and Conditions
+          </label>
+        </div>
+        <div className="text-xs flex gap-1 text-[#dedfeb] ">
+          Dont Have an Account ?{" "}
+          <span className="text-[#CACA8F] cursor-pointer font-semibold">
+            SignUp
+          </span>
+        </div>
+        <div className="transition-transform duration-150 active:scale-95">
+          <input
+            className="bg-transparent font-medium text-md cursor-pointer tracking-wider bg-[#383a46] focus:outline-none hover:bg-[#8F8FCA] hover:text-[#383a46] text-[#fff] px-6 py-1 rounded-lg login-box"
+            type="submit"
+            value="Login"
+            onClick={(e) => {
+              e.preventDefault();
+              setIsLogin(false);
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,33 @@
 import { useState } from "react";
 import Chat from "./chat";
 import ImageOverlay from "./imageOverlay";
+import Login from "./Login";
 
 export default function Home() {
+  const [isLogin, setIsLogin] = useState(true);
   const ProfilePicture =
     "https://media-hyd1-1.cdn.whatsapp.net/v/t61.24694-24/379619630_6949750128379753_6172259427711619252_n.jpg?ccb=11-4&oh=01_Q5AaIO2kBGB6JkOO1GOwTcvG9XIDmY-5Vitx5Oo9Mv7P_lPp&oe=674A8A31&_nc_sid=5e03e0&_nc_cat=108";
 
   const [showImageOverlay, setShowImageOverlay] = useState(false);
   return (
     <>
-      <div className="relative">
-        {showImageOverlay && (
-          <div
-            className="w-full h-full bg-[rgba(25,26,34,0.9)] absolute top-0 right-0 flex justify-center items-center z-50"
-            onClick={() => {
-              setShowImageOverlay(false);
-            }}
-          >
-            <ImageOverlay src={ProfilePicture} />
-          </div>
-        )}
-        <Chat setShowImageOverlay={setShowImageOverlay} />
-      </div>
+      {isLogin ? (
+        <Login setIsLogin={setIsLogin} />
+      ) : (
+        <div className="relative">
+          {showImageOverlay && (
+            <div
+              className="w-full h-full bg-[rgba(25,26,34,0.9)] absolute top-0 right-0 flex justify-center items-center z-50"
+              onClick={() => {
+                setShowImageOverlay(false);
+              }}
+            >
+              <ImageOverlay src={ProfilePicture} />
+            </div>
+          )}
+          <Chat setShowImageOverlay={setShowImageOverlay} />
+        </div>
+      )}
     </>
   );
 }
