1dde8c824e63ed909a4ad248132ee941564db848
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-20 14:47:07 +0530

    added banner pic and logo improve )

--- src/app/ContactHeader.tsx
+++ src/app/ContactHeader.tsx
@@ -11,8 +11,17 @@ function ContactHeader() {
     <div
       className={`flex items-center justify-start  font-semibold  gap-6 p-4 `}
     >
-      <span className={`${kanit.className} text-5xl  text-[#CACA8F]`}>
+      <span className={`${kanit.className} text-5xl flex gap-3 text-[#CACA8F]`}>
         Ping
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+        >
+          <path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
+        </svg>
       </span>
       <span className="grow"></span>
       <span>
--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -10,14 +10,15 @@ interface ProfileProps {
 function Profile({ setIsProfileOpen, setShowImageOverlay }: ProfileProps) {
   const ProfilePicture =
     "https://media-hyd1-1.cdn.whatsapp.net/v/t61.24694-24/379619630_6949750128379753_6172259427711619252_n.jpg?ccb=11-4&oh=01_Q5AaIO2kBGB6JkOO1GOwTcvG9XIDmY-5Vitx5Oo9Mv7P_lPp&oe=674A8A31&_nc_sid=5e03e0&_nc_cat=108";
+  const BannerPic =
+    "https://media.licdn.com/dms/image/v2/D4D16AQGiuVbiEdaPAg/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1711031992791?e=1737590400&v=beta&t=L6veoyCxeddYWy2STt-4ABQGsbXWvZzwMzWNluS7xok";
   return (
     <>
       <div className="w-full h-full relative  ">
         <div
-          className="flex flex-col bg-[rgba(143,143,202,0.1)] h-[30%] relative bg-no-repeat bg-cover"
+          className="flex flex-col bg-[rgba(143,143,202,0.1)] h-[30%] relative bg-no-repeat bg-center bg-cover"
           style={{
-            backgroundImage: `url(${ProfilePicture},
-)`,
+            backgroundImage: `url(${BannerPic})`,
           }}
         >
           <div className="flex gap-4 w-full px-4 py-2 bg-transparent">
@@ -47,7 +48,7 @@ function Profile({ setIsProfileOpen, setShowImageOverlay }: ProfileProps) {
             </svg>
           </div>
           <Image
-            className="rounded-full  absolute bottom-[-40%] left-4 border-4 z-20"
+            className="rounded-full  absolute bottom-[-40%] left-4 border-[6px] z-20 border-[#191a22] "
             onClick={() => {
               setShowImageOverlay(true);
             }}
@@ -77,7 +78,7 @@ function Profile({ setIsProfileOpen, setShowImageOverlay }: ProfileProps) {
             <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" />
           </svg>
         </div>
-        <div className="font-bold  text-xl flex gap-4  items-center px-8">
+        <div className="font-bold  text-xl flex gap-4 text-[#CACA8F]  items-center px-8">
           Nischal_Mantri{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -89,7 +90,14 @@ function Profile({ setIsProfileOpen, setShowImageOverlay }: ProfileProps) {
             <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
           </svg>
         </div>
-        <div className="text-xs text-[#adaeb7] pl-8">@nischal_mantri </div>
+        <div className="text-xs  text-[#adaeb7]  pl-8">@nischal_mantri </div>
+        <div className=" pl-8 pt-4 flex flex-col gap-1 border-b-2 py-10">
+          <div className="text-[#8888d5] text-md">Bio</div>
+          <div className="text-xs font-medium  text-[#adaeb7]">
+            Life is like riding a bicycle. To keep your balance, you must keep
+            moving.
+          </div>
+        </div>
       </div>
     </>
   );
