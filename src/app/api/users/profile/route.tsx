13553b04709fadc1002dd87cc93c1c2f189688a6
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-22 21:22:31 +0530

    profile path , User is returned (-password )  used getTokenData

---
+++ src/app/api/users/profile/route.tsx
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from "server/server";
+import User from "server/UserModal";
+import { getTokenData } from "src/app/helper/getTokenData";
+connectDB();
+
+export async function POST(request: NextRequest) {
+  const userId = await getTokenData(request);
+  User.findById({ _id: userId }).select("-password");
+  console.log(userId);
+  return NextResponse.json({ message: "User Found", data: User });
+}
