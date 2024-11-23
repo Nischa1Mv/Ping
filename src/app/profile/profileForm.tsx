8f6c8f0027729515f865a773230816358fdea717
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-23 15:29:23 +0530

    when clciked save redirect to /

--- src/app/profile/profileForm.tsx
+++ src/app/profile/profileForm.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ProfileFormProps {
@@ -20,6 +22,7 @@ function ProfileForm({
   setProfilePicture,
   setBannerPicture,
 }: ProfileFormProps) {
+  const router = useRouter();
   return (
     <div className="flex flex-col gap-10 items-center px-10 justify-center h-full text-[#adaeb7]">
       <div className="w-full  flex items-center justify-center text-2xl font-semibold">
@@ -97,6 +100,9 @@ function ProfileForm({
       </div>
       <div>
         <button
+          onClick={() => {
+            router.push("/");
+          }}
           value="save"
           className="bg-transparent hover:font-bold cursor-pointer tracking-wider focus:outline-none hover:bg-[#8F8FCA] hover:text-[#383a46] text-[#fff] px-6 py-1 rounded-lg login-box active:scale-95 transition-transform duration-150 active:bg-[#9696cd]  "
         >
