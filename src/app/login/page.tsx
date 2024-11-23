d997f60e9e670ad156a5022357f538121693d44d
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-23 22:33:36 +0530

    connected backend to the login as well

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Kanit } from "next/font/google";
 import { useRouter } from "next/navigation";
-import { useLogin } from "../Context";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Link from "next/link";
+
 const kanit = Kanit({
   subsets: ["latin"],
   weight: ["600"],
@@ -11,83 +14,122 @@ const kanit = Kanit({
 
 function Login() {
   const router = useRouter();
-  const handleLogin = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setIsLogin(true);
-    router.push("/");
-  };
-  useEffect(() => {
-    isLogin && router.push("/");
-  }, []);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-  const { isLogin, setIsLogin } = useLogin();
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      setIsDisabled(true);
+      setIsLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("LoggedIn", response.data);
+      router.push("/");
+      toast.success("User Is Logged In");
+    } catch (error: any) {
+      console.log("coudnt sign up", error);
+      toast.error("Coudn't Sign up.");
+    }
+  };
+
+  useEffect(() => {
+    //if email and password are not empty, enable the button
+    if (user.email.length > 0 && user.password.length > 0) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [user]);
+
   return (
     <div className="h-screen bg-[#191a22] flex justify-center items-center">
-      <form className="flex flex-col justify-center items-center gap-7 w-[35%] py-10 rounded-lg bg-[#21222e] border-[#1E1E1E]">
-        <span
-          className={`${kanit.className} text-6xl gap-1 flex text-[#CACA8F]`}
-        >
-          Ping
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="currentColor"
+      {isLoading ? (
+        <>
+          {" "}
+          <div
+            className={`${kanit.className} text-xl  text-[#CACA8F] tracking-widest`}
           >
-            <path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
-          </svg>
-        </span>
-
-        <div className="text-xl font-bold text-[#8F8FCA] italic px-4 gap-2 items-center flex">
-          Login
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#8F8FCA"
-          >
-            <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
-          </svg>
-        </div>
-
-        <div>
-          <input
-            className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 border-[#dedfeb] focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
-            type="email"
-            placeholder="Email"
-          />
-        </div>
-        <div>
-          <input
-            className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="text-xs flex gap-1 text-[#dedfeb] ">
-          Dont Have an Account ?{" "}
+            Loading....
+          </div>
+        </>
+      ) : (
+        <form className="flex flex-col justify-center items-center gap-7 w-[35%] py-10 rounded-lg bg-[#21222e] border-[#1E1E1E]">
           <span
-            className="text-[#CACA8F] cursor-pointer font-semibold"
-            onClick={() => {
-              router.push("/signup");
-            }}
+            className={`${kanit.className} text-6xl gap-1 flex text-[#CACA8F]`}
           >
-            SignUp
+            Ping
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="currentColor"
+            >
+              <path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
+            </svg>
           </span>
-        </div>
-        <div className="transition-transform duration-150 active:scale-95">
-          <input
-            className="bg-transparent font-medium text-md cursor-pointer tracking-wider bg-[#383a46] focus:outline-none hover:bg-[#8F8FCA] hover:text-[#383a46] text-[#fff] px-6 py-1 rounded-lg login-box"
-            type="submit"
-            value="Login"
-            onClick={(e) => {
-              handleLogin(e);
-            }}
-          />
-        </div>
-      </form>
+
+          <div className="text-xl font-bold text-[#8F8FCA] italic px-4 gap-2 items-center flex">
+            Login
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#8F8FCA"
+            >
+              <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z" />
+            </svg>
+          </div>
+
+          <div>
+            <input
+              value={user.email}
+              onChange={(e) => {
+                setUser({ ...user, email: e.target.value });
+              }}
+              className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 border-[#dedfeb] focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <input
+              value={user.password}
+              onChange={(e) => {
+                setUser({ ...user, password: e.target.value });
+              }}
+              className="w-[250px] bg-transparent border-b-2 tracking-wide focus:outline-none focus:border-b-2 focus:border-b-[#CACA8F] text-white px-2 pt-2 placeholder:text-{#dedfeb]"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="text-xs flex gap-1 text-[#dedfeb] ">
+            Dont Have an Account ?{" "}
+            <Link
+              className="text-[#CACA8F] cursor-pointer font-semibold"
+              href={`/login`}
+            >
+              SignUp
+            </Link>
+          </div>
+          <div className="transition-transform duration-150 active:scale-95">
+            <input
+              className="bg-transparent font-medium text-md cursor-pointer tracking-wider bg-[#383a46] focus:outline-none hover:bg-[#8F8FCA] hover:text-[#383a46] text-[#fff] px-6 py-1 rounded-lg login-box"
+              type="submit"
+              value="Login"
+              onClick={(e) => {
+                e.preventDefault();
+                handleLogin();
+              }}
+            />
+          </div>
+        </form>
+      )}
     </div>
   );
 }
