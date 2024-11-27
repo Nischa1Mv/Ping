36f0a0b7224d4f343cd287b4f96320fd3827e970
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-27 12:21:34 +0530

    ui centered the content

--- src/app/contact/ContactHeader.tsx
+++ src/app/contact/ContactHeader.tsx
@@ -11,7 +11,7 @@ const kanit = Kanit({
 function ContactHeader() {
   const [isSearch, setIsSearch] = useState(false);
   const [showFriends, setShowFriends] = useState(false);
-  const [FriendRequests, setFriendRequest] = useState(false);
+  const [FriendRequests, setFriendRequest] = useState(true);
 
   return (
     <div
@@ -73,7 +73,7 @@ function ContactHeader() {
         {showFriends && (
           <div
             style={{ boxShadow: "rgba(149, 157, 165, 0.3) 0px 0px 10px" }}
-            className="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 
+            className="absolute left-1/2 transform -translate-x-1/2 top-full mt-2  items-center
              h-[400px] w-[300px] overflow-x-hidden overflow-y-auto flex flex-col 
              gap-4 py-2 z-50 px-4 bg-[rgba(33,34,46,0.9)] rounded-xl scrollbar-default "
           >
