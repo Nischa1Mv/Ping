0aaf320502908147eab15432c6f71d1c9d87c7ac
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-27 18:40:59 +0530

    sent are wokring but received is not

--- src/app/api/friend/request/sent/route.tsx
+++ src/app/api/friend/request/sent/route.tsx
@@ -11,14 +11,26 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   try {
-    const response = await FriendRequest.find({ sender: userId });
+    const response = await FriendRequest.find({ sender: userId }).populate(
+      "receiver",
+      "username profilePicture"
+    );
+
     if (response.length === 0) {
       return NextResponse.json(
         { message: "No friend request sent" },
         { status: 404 }
       );
     }
-    return NextResponse.json(response, { status: 200 });
+
+    const friendRequests = response.map((friendRequest) => {
+      return {
+        id: friendRequest._id,
+        username: friendRequest.receiver.username,
+        profilePicture: friendRequest.receiver.profilePicture,
+      };
+    });
+    return NextResponse.json(friendRequests, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
