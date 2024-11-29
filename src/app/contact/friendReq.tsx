d68ea7f51a53a6977a9c1ac493891ad82a4ce91b
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-29 21:10:20 +0530

    adjusted the req notifications ui for now

--- src/app/contact/friendReq.tsx
+++ src/app/contact/friendReq.tsx
@@ -17,8 +17,12 @@ function FriendReq({
   handleAction,
 }: friendReqProps) {
   return (
-    <div className="flex flex-col w-[85%] bg-[#191a22] py-2 px-4 rounded-xl m-auto">
-      <li className="flex gap-4 hover:text-[#caca8f] py-2 cursor-pointer text-xs bg-[#191a22] hover:bg-[#1b1c29] px-4 rounded-lg  items-center font-semibold tracking-wider ">
+    <div
+      className={` hover:text-[#caca8f] ${
+        !sent ? " flex-col flex" : "flex items-center "
+      }  w-[85%] bg-[#191a22] py-2 px-4 rounded-xl my-2 `}
+    >
+      <li className="flex gap-4 hover:text-[#caca8f] py-1 cursor-pointer text-xs bg-[#191a22] px-2 rounded-lg  items-center font-semibold tracking-wider ">
         <Image
           className="rounded-full aspect-square"
           src={profilePicture || "/default-profile-picture.jpg"}
@@ -28,7 +32,7 @@ function FriendReq({
         />
         @ {username}
       </li>
-      {!sent && (
+      {!sent ? (
         <div className="flex gap-4 text-sm">
           <button
             onClick={() => {
@@ -47,6 +51,18 @@ function FriendReq({
             Reject
           </div>
         </div>
+      ) : (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="18px"
+            viewBox="0 -960 960 960"
+            width="18px"
+            fill="#C14D3A"
+          >
+            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+          </svg>
+        </>
       )}
     </div>
   );
