ea8b444e2944e2502823a47320c0f9c191abedea
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-11-30 21:07:36 +0530

    remove chat temporary is done

--- src/app/contact/Friendsmsg.tsx
+++ src/app/contact/Friendsmsg.tsx
@@ -8,16 +8,24 @@ const kanit = Kanit({
   style: ["italic"],
 });
 interface FriendsmsgProps {
+  chatId: string;
   name: string;
   message?: string;
   avatar?: string;
   username?: string;
+  removeChat: () => void;
 }
 
-function Friendsmsg({ name, message, avatar, username }: FriendsmsgProps) {
+function Friendsmsg({
+  name,
+  message,
+  avatar,
+  username,
+  removeChat,
+}: FriendsmsgProps) {
   return (
-    <li className=" cursor-pointer rounded-lg py-2 px-4 bg-[#1f2029] flex gap-4 items-center hover:bg-[#444479] ">
-      <div className="min-w-[55px] aspect-square">
+    <li className=" cursor-pointer rounded-lg  pl-4 bg-[#1f2029] flex gap-4 items-center hover:bg-[#444479] ">
+      <div className="min-w-[55px] aspect-square py-2">
         <Image
           src={avatar || "/avatar.jpg"}
           width={55}
@@ -26,16 +34,36 @@ function Friendsmsg({ name, message, avatar, username }: FriendsmsgProps) {
           className="rounded-full"
         />
       </div>
-      <div className="flex flex-col ">
+      <div className="flex flex-col w-full overflow-hidden pb-2 ">
         <h4
-          className={`text-lg font-semibold ${kanit.className} text-[#CACA8F]`}
+          className={` relative text-lg font-semibold ${kanit.className} text-[#CACA8F]`}
         >
           {name}
-          <span className="text-xs font-light text-gray-500">@{username}</span>
+          <span className="text-xs font-light text-gray-500 justify-end absolute right-0 top-0 bottom-0 ">
+            @{username}
+          </span>
         </h4>
-        <p className="text-[0.65rem] tracking-wide line-clamp-2 text-gray-300 overflow-hidden text-ellipsis max-h-[50px]">
+        <p className="text-[0.65rem] tracking-wide line-clamp-2 text-gray-300 overflow-hidden text-ellipsis max-h-[50px] break-words">
           {message}
         </p>
+      </div>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          removeChat();
+        }}
+        className="relative px-1 rounded-r-lg rounded-l-sm flex text-red-300 h-full items-center justify-center bg-transparent hover:bg-red-600 transition-all duration-300 ease-out transform"
+      >
+        <div className="absolute inset-0 bg-red-300 transform scale-x-0 origin-right hover:scale-x-100 transition-all duration-500 "></div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="18px"
+          viewBox="0 -960 960 960"
+          width="18px"
+          fill="currentColor"
+        >
+          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+        </svg>
       </div>
     </li>
   );
--- src/app/contact/FrndList.tsx
+++ src/app/contact/FrndList.tsx
@@ -9,35 +9,54 @@ function FrndList() {
   const [conversations, setConversations] = useState<Conversation[]>([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchConversations = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get<Conversation[]>(
-          "/api/conversation/current"
-        );
-        console.log("Conversations:", response.data);
-        setConversations(response.data);
-        toast.success("Conversations fetched successfully");
-      } catch (err) {
-        console.error("Error fetching conversations:", err);
-        toast.error("Error fetching conversations");
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchConversations = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get<Conversation[]>(
+        "/api/conversation/current"
+      );
+      console.log("Conversations:", response.data);
+      setConversations(response.data);
+      toast.success("Conversations fetched successfully");
+    } catch (err) {
+      console.error("Error fetching conversations:", err);
+      toast.error("Error fetching conversations");
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchConversations();
   }, []);
+
+  const removeChat = async (chatId: string) => {
+    try {
+      const response = await axios.post("/api/conversation/remove", {
+        conversationId: chatId,
+      });
+      console.log("Chat deleted", response.data);
+      fetchConversations();
+      toast.success("Chat deleted successfully");
+    } catch (err) {
+      console.error("Error deleting chat:", err);
+      toast.error("Error deleting chat");
+    }
+  };
 
   return (
     <ul className="h-full px-4 py-2 flex flex-col gap-3 overflow-y-auto scrollbar-hide">
       {conversations.map((chat) => (
         // You may need to adjust this part depending on how your `participantDetails` are structured
         <Friendsmsg
+          removeChat={() => removeChat(chat._id)}
           key={chat._id}
+          chatId={chat._id}
           name={chat.participantDetails[0]?.displayName || "known User"}
-          message={chat.messages[0]?.content || "No messages"}
+          message={
+            chat.messages[0]?.content ||
+            "No messages yet, start the conversation!"
+          }
           avatar={chat.participantDetails[0]?.profilePicture}
           username={chat.participantDetails[0]?.username || "knownUser"}
         />
