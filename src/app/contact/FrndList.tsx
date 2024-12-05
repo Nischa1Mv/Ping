e1cefe05dd88573259ee570c54478ec4087162c4
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-12-05 17:15:07 +0530

    added laoding animaiton while loading coversations

--- src/app/contact/FrndList.tsx
+++ src/app/contact/FrndList.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Friendsmsg from "./Friendsmsg";
 import { Conversation } from "./types";
 import toast from "react-hot-toast";
 import { useChat } from "../Context";
+import { set } from "mongoose";
+import { Convergence } from "next/font/google";
 
 interface FrndListProps {
   conversations: Conversation[];
@@ -45,29 +47,38 @@ function FrndList({
   };
 
   return (
-    <ul className="h-full px-4 py-2 flex flex-col gap-3 overflow-y-auto scrollbar-hide">
-      {message && (
-        <div className="h-full items-center justify-center flex flex-col gap-3">
-          <p>{message}</p>
-          <p>Click On the Search Icon to Start a Conversation</p>
+    <>
+      {loading ? (
+        <div className="flex items-center space-x-2 text-gray-700 justify-center h-full">
+          <div className="w-5 h-5 border-4 border-t-transparent border-[#8f8fca] rounded-full animate-spin"></div>
+          <div>Loading</div>
         </div>
+      ) : (
+        <ul className="h-full px-4 py-2 flex flex-col gap-3 overflow-y-auto scrollbar-hide">
+          {message && (
+            <div className="h-full items-center justify-center flex flex-col gap-3">
+              <p>{message}</p>
+              <p>Click On the Search Icon to Start a Conversation</p>
+            </div>
+          )}
+          {conversations.map((chat) => (
+            <Friendsmsg
+              removeChat={() => removeChat(chat._id)}
+              key={chat._id}
+              chatId={chat._id}
+              name={chat.participantDetails[0]?.displayName || "known User"}
+              message={
+                chat.messages[0]?.content ||
+                "No messages yet, start the conversation!"
+              }
+              avatar={chat.participantDetails[0]?.profilePicture}
+              username={chat.participantDetails[0]?.username || "knownUser"}
+              openChat={(chatId) => openChat(chatId)}
+            />
+          ))}
+        </ul>
       )}
-      {conversations.map((chat) => (
-        <Friendsmsg
-          removeChat={() => removeChat(chat._id)}
-          key={chat._id}
-          chatId={chat._id}
-          name={chat.participantDetails[0]?.displayName || "known User"}
-          message={
-            chat.messages[0]?.content ||
-            "No messages yet, start the conversation!"
-          }
-          avatar={chat.participantDetails[0]?.profilePicture}
-          username={chat.participantDetails[0]?.username || "knownUser"}
-          openChat={(chatId) => openChat(chatId)}
-        />
-      ))}
-    </ul>
+    </>
   );
 }
 
